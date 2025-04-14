5fea859224f0ad3682fa2f8ccac1a73579daab3f
megidogem <hibarioath@proton.me>
2025-04-14 12:45:25 +0200

    sway-cosas

--- sway.nix
+++ sway.nix
@@ -1,22 +1,37 @@
 { config, pkgs, lib, ... }:
 {
 
-  programs.sway = {
+  programs = {
+    nm-applet.enable = true;
+    sway = {
+      enable = true;
+      wrapperFeatures.gtk = true;
+      extraOptions = [ "--unsupported-gpu" ];
+      extraPackages = with pkgs; [
+        adwaita-icon-theme
+        alacritty
+        bemenu
+        jq
+        gammastep
+        grim
+        i3status
+        mako
+        nwg-look
+        slurp
+        swayidle
+        swayimg
+        swaylock
+        zathura
+      ];
+      #package = pkgs.swayfx;
+    };
+    yazi.enable = true;
+  };
+
+  xdg.portal = {
+    wlr.enable = true;
     enable = true;
-    wrapperFeatures.gtk = true;
-    extraOptions = [ "--unsupported-gpu" ];
-    extraPackages = with pkgs; [
-      alacritty
-      bemenu
-      jq
-      gammastep
-      grim
-      i3status
-      mako
-      nwg-look
-      slurp
-    ];
-    #package = pkgs.swayfx;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
 
   services.gnome.gnome-keyring.enable = true;
